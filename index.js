f6c95c9e77ea9b84e9ecfa8ccc1dbeb4276c50fd
Mohuya748 <khandakermohuya@gmail.com>
2022-05-26 01:03:00 +0600

    user added

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ async function run() {
   try {
     await client.connect();
     const partsCollection = client.db('parts-manufacture').collection('parts');
+    const userCollection = client.db('parts-manufacture').collection('users');
+    const bookingsCollection = client.db('parts-manufacture').collection('bookings');
 
     app.get('/parts', async (req, res) => {
       const query = {};
@@ -34,6 +36,54 @@ async function run() {
 
     });
 
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    })
+
+     // update item quantity 
+    //  app.put('/parts/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const updateItem = req.body;
+    //   console.log(id, updateItem)
+    //   const filter = { _id: ObjectId(id) };
+    //   console.log(filter)
+    //   const options = { upsert: true };
+    //   const updatedDoc = {
+    //     $set: {
+    //       available_Quantity : updateItem.quantity,
+    //     }
+    //   };
+    //   console.log(updatedDoc)
+    //   const result = await partsCollection.updateOne(filter, updatedDoc, options);
+    //   res.send(result);
+
+    // })
+
+    app.get('/booking', async(req, res) =>{
+      const email = req.query.email;
+      const query = {email: email};
+      console.log("new  item",email);
+      const result = await bookingsCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    // POST: add a booking
+    app.post('/booking', async (req, res) => {
+      const newbooking = req.body;
+      console.log("adding new  item", newbooking);
+      const result = await bookingsCollection.insertOne(newbooking);
+      res.send(result);
+    })
+
+   
   }
   finally {
 
